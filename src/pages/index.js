7f30546a3221f61fccb9ceaae59bf066ceddaffe
Zachary Kirby <zkirby@gmail.com>
2021-09-04 15:35:06 -0700

    fxi config

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import ky from 'ky';
 
 const IndexPage = () => {
 	const createPage = async () => {
-		console.log(await ky.get('/api/hello-world'));
+		console.log(await ky.get('amovieaminute.gatsbyjs.io/api/hello-world'));
 	};
 
 	return <div><h1>A movie a minute</h1><button onClick={createPage}>create a page</button></div>;
