b1ec753a6fc3baf72107574936df48e38f6b2bd1
Zachary Kirby <zkirby@gmail.com>
2021-09-04 15:31:56 -0700

    added api function

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,13 @@
 import * as React from 'react';
-import { Client } from '@notionhq/client';
-
-const notion = new Client({ auth: 'Secret_EF3gQEg7xh9FejTTZuzJrWfwPk5kfMM2XmpUvu3Tnze' });
+import ky from 'ky';
 
 const IndexPage = () => {
-	React.useEffect(async () => {
-		const response = await notion.pages.create({
-			parent: { database_id: 'c6de34cdadb34feebc7d1b7602014ad6' },
-			properties: {
-				title: {
-					title: [
-						{
-							text: {
-								content: 'hellllooooo',
-							},
-						},
-					],
-				},
-			},
-		});
+	const createPage = async () => {
+		console.log(await ky.get('/api/hello-world'));
+	};
 
-		console.log(response);
-	});
-
-	return <h1>A movie a minute</h1>;
+	return <div><h1>A movie a minute</h1><button onClick={createPage}>create a page</button></div>;
 };
 
 export default IndexPage;
-// Secret_EF3gQEg7xh9FejTTZuzJrWfwPk5kfMM2XmpUvu3Tnze
-// c6de34cdadb34feebc7d1b7602014ad6
+
