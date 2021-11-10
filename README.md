0b0ca55a964fb0797fc1db9d32e8164b1051f78c
zkirby <zkirby16@gmail.com>
2021-11-09 22:17:15 -0800

    add inital about page

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 Home grown movie blog. 
 
-TODO:
-- [ ] Skeleton loaders
+TODO for launch:
 - [ ] Caching the API response for fast loading
 - [ ] Adding the about page
 - [ ] Eventually add this: https://mailchimp.com/
 - [ ] Analytics as well
 - [ ] http://www.omdbapi.com/
+- [ ] Polish everything
---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import PageWrapper from "../features/shared/PageWrapper"
+
+const IndexPage = () => (
+  <PageWrapper className="home" pageName={"About"}>
+    <div>
+      Where I'll ultamtely pass my judgement, but that's all it is, my personal
+      judgement.
+    </div>
+  </PageWrapper>
+)
+
+export default IndexPage
